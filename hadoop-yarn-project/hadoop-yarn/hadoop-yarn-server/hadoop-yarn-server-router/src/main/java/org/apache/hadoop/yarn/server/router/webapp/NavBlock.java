f395eb95415a511271f7293584dc8127bbaedde8
slfan1989 <louj1988@@>
2022-09-28 09:33:32 +0800

    YARN-99997. Fix CheckStyle.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-router/src/main/java/org/apache/hadoop/yarn/server/router/webapp/NavBlock.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-router/src/main/java/org/apache/hadoop/yarn/server/router/webapp/NavBlock.java
@@ -18,6 +18,10 @@
 
 package org.apache.hadoop.yarn.server.router.webapp;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.yarn.api.records.YarnApplicationState;
+import org.apache.hadoop.yarn.server.webapp.WebPageUtils;
+import org.apache.hadoop.yarn.webapp.hamlet2.Hamlet;
 import org.apache.hadoop.yarn.webapp.view.HtmlBlock;
 
 /**
@@ -27,20 +31,42 @@ public class NavBlock extends HtmlBlock {
 
   @Override
   public void render(Block html) {
-    html.
-      div("#nav").
-        h3("Cluster").
-        ul().
-          li().a(url(""), "About").__().
-          li().a(url("federation"), "Federation").__().
-          li().a(url("nodes"), "Nodes").__().
-          li().a(url("apps"), "Applications").__().
+
+    Hamlet.UL<Hamlet.DIV<Hamlet>> mainList =
+            html.div("#nav").
+            h3("Cluster").
+            ul().
+            li().a(url(""), "About").__().
+            li().a(url("federation"), "Federation").__().
+            li().a(url("nodes"), "Nodes").__();
+
+    Hamlet.UL<Hamlet.LI<Hamlet.UL<Hamlet.DIV<Hamlet>>>> subAppsList = mainList.
+            li().a(url("apps"), "Applications").
+            ul();
+    subAppsList.li().__();
+
+    for (YarnApplicationState state : YarnApplicationState.values()) {
+      subAppsList.
+              li().a(url("apps", state.toString()), state.toString()).__();
+    }
+
+    subAppsList.__().__();
+
+    Hamlet.DIV<Hamlet> sectionBefore = mainList.__();
+    Configuration conf = new Configuration();
+    Hamlet.UL<Hamlet.DIV<Hamlet>> tools = WebPageUtils.appendToolSection(sectionBefore, conf);
+
+    if (tools == null) {
+      return;
+    }
+
+    tools.__().__();
+
+    /*li().a(url("apps"), "Applications").__().
       __().
       h3("Tools").
-      ul().
-        li().a("/conf", "Configuration").__().
-        li().a("/logs", "Local logs").__().
+      ul().li().a("/conf", "Configuration").__().li().a("/logs", "Local logs").__().
         li().a("/stacks", "Server stacks").__().
-        li().a("/jmx?qry=Hadoop:*", "Server metrics").__().__().__();
+        li().a("/jmx?qry=Hadoop:*", "Server metrics").__().__().__();*/
   }
 }
